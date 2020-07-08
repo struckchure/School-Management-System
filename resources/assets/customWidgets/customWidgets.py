7cdb79a2a7051dc8626c1be2fb52e15b3de8432a
Mohammed2702 <ameenmohammed2311@gmail.com>
2020-07-08 18:30:30 +0100

    First Row, Second Row - Solomon

--- resources/assets/customWidgets/customWidgets.py
+++ resources/assets/customWidgets/customWidgets.py
@@ -269,3 +269,152 @@ class PageCrumb(QLabel):
 		# self.setMaximumSize(100, 45)
 		self.setFixedSize(500, 45)
 		self.setObjectName('pageCrumb')
+
+
+class FirstRow(QPushButton):
+	def __init__(self, head, total_no, icon='resources/assets/images/icons/group.png'):
+		QPushButton.__init__(self)
+
+		self.hd = head
+		self.tln = total_no
+		self.icn = icon
+
+		blurradius = 160
+		offset = 0.1
+		color = QColor(0, 0, 0, 255 * .3)
+
+		self.shadow_effect = QGraphicsDropShadowEffect()
+		self.shadow_effect.setBlurRadius(blurradius)
+		self.shadow_effect.setOffset(offset)
+		self.shadow_effect.setColor(color)
+		self.setGraphicsEffect(self.shadow_effect)
+
+		width = 230
+		height = 70
+
+		qss = open('resources/assets/qss/boostrap.qss', 'r').read()
+		self.setStyleSheet(qss)
+		self.setContentsMargins(0, 0, 0, 0)
+		self.setMinimumSize(width, height)
+		self.setObjectName('firstRow')
+		self.setIcon(QIcon(self.icn))
+		self.setIconSize(QSize(50, 50))
+
+		self.assist_layout = QHBoxLayout()
+		self.assist_layout.setContentsMargins(0, 0, 0, 0)
+
+		self.firstRow = QVBoxLayout()
+		self.firstRow.setSpacing(0)
+		self.firstRow.setContentsMargins(0, 0, 0, 0)
+
+		self.head = QLabel(str(self.hd))
+		self.head.setMaximumHeight(11)
+		self.head.setObjectName("headText")
+		self.firstRow.addWidget(self.head)
+
+		self.total = QLabel(str(self.tln))
+		self.total.setMaximumHeight(20)
+		self.total.setObjectName("totalText")
+		self.firstRow.addWidget(self.total)
+
+		self.assist_layout.addLayout(self.firstRow)
+
+		self.box_icon = QLabel()
+		self.box_icon.setAlignment(Qt.AlignRight)
+		self.box_icon.setContentsMargins(0, 15, 24, 0)
+		box_icon = QPixmap(self.icn)
+		self.box_icon.setPixmap(box_icon)
+		#self.assist_layout.addWidget(self.box_icon)
+
+		self.setLayout(self.assist_layout)
+
+
+class SecondRow(QGroupBox):
+	def __init__(self, title, eventName):
+		QGroupBox.__init__(self)
+		self.eventName = eventName
+		self.title = title
+
+		blurradius = 200
+		offset = 0.1
+		color = QColor(0, 0, 0, 255 * .3)
+
+		self.shadow_effect = QGraphicsDropShadowEffect()
+		self.shadow_effect.setBlurRadius(blurradius)
+		self.shadow_effect.setOffset(offset)
+		self.shadow_effect.setColor(color)
+		self.setGraphicsEffect(self.shadow_effect)
+
+		width = 480
+
+		qss = open('resources/assets/qss/boostrap.qss', 'r').read()
+		self.setStyleSheet(qss)
+		self.setContentsMargins(0, 0, 0, 0)
+		self.setMaximumWidth(width)
+		self.setObjectName('secondRow')
+
+		self.secondRow = QVBoxLayout()
+		self.secondRow.setAlignment(Qt.AlignTop)
+		self.secondRow.setSpacing(0)
+		self.secondRow.setContentsMargins(0, 0, 0, 0)
+
+		#layout for title and add button
+		self.box_heading = QHBoxLayout()
+		self.box_heading.setSpacing(0)
+		self.box_heading.setContentsMargins(0, 0, 0, 0)
+
+		self.box_title = QLabel(self.title)
+		self.box_title.setMinimumSize(440, 35)
+		self.box_title.setObjectName('boxTitle')
+		#self.box_title.setAlignment(Qt.AlignTop)
+		self.box_heading.addWidget(self.box_title)
+
+		self.add_button = QPushButton()
+		self.add_button.setIcon(QIcon('resources/assets/images/icons/add_icon.png'))
+		self.add_button.setIconSize(QSize(20, 20))
+		self.add_button.setMinimumHeight(35)
+		#self.add_button.clicked.connect(self.addEvent)
+		self.add_button.setObjectName('addButton')
+		self.box_heading.addWidget(self.add_button)
+
+		self.secondRow.addLayout(self.box_heading)
+
+		self.eventList(self.eventName)
+
+		self.setLayout(self.secondRow)
+
+	def eventList(self, eventName):
+		# layout for lists
+		self.list_layout = QHBoxLayout()
+		self.list_layout.setSpacing(0)
+		self.list_layout.setContentsMargins(0, 0, 0, 0)
+
+		height = 30
+
+		self.event_name = QLabel(str(eventName))
+		self.event_name.setMinimumHeight(height)
+		self.event_name.setWordWrap(True)
+		self.event_name.setObjectName('event')
+		self.list_layout.addWidget(self.event_name)
+
+		self.edit_list_btn = QPushButton()
+		self.edit_list_btn.setMaximumSize(30, height)
+		self.edit_list_btn.setIcon(QIcon('resources/assets/images/icons/edit_icon.png'))
+		self.edit_list_btn.setIconSize(QSize(15, 15))
+		self.edit_list_btn.setObjectName('listBtn')
+		self.list_layout.addWidget(self.edit_list_btn)
+
+		self.delete_list_btn = QPushButton()
+		self.delete_list_btn.setMaximumSize(30, height)
+		self.delete_list_btn.setIcon(QIcon('resources/assets/images/icons/delete_icon.png'))
+		self.delete_list_btn.setIconSize(QSize(15, 15))
+		self.delete_list_btn.setObjectName('listBtn')
+		self.list_layout.addWidget(self.delete_list_btn)
+
+		self.event_scroll = QScrollArea()
+		self.event_scroll.setMaximumHeight(50)
+		self.event_scroll.setWidgetResizable(True)
+		self.event_scroll.setObjectName('eventScroll')
+		self.event_scroll.setLayout(self.list_layout)
+
+		self.secondRow.addWidget(self.event_scroll)
