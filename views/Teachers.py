b79ce40e562d2db977110d62e5f70f725494045d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2020-09-23 12:13:31 +0100

    added teacher's registration

--- views/Teachers.py
+++ views/Teachers.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import *
 from views import utils
 from resources.assets.customWidgets import customWidgets
 from views import pageConfigurations
+from Home import models
 
 # Custom Modules End
 
@@ -58,22 +59,23 @@ class Teachers(QGroupBox):
 		self.groupLayout.addWidget(self.teacherTables)
 
 	def getAllTeachers(self):
-		# for teacher in allTeachers:
-		# 	pass
+		allTeachers = models.User.objects.all().filter(is_teacher=True)
+		# allTeachers = models.User.objects.all()
 
-		self.teacherTables.addRow(
-			[
-				'Mohammed',
-				'Ameen',
-				'MD',
-				'ameenmohammed2311@gmail.com',
-				'12/07/20'
-			],
-			[
-				('View', 'black', 'User', 'view'),
-				('Delete', 'orange', 'User', 'delete'),
-			]
-		)
+		for teacher in allTeachers:
+			self.teacherTables.addRow(
+				[
+					teacher.first_name,
+					teacher.last_name,
+					teacher.username,
+					teacher.email,
+					teacher.date_joined.strftime('%B %d, %Y')
+				],
+				[
+					('View', 'black', 'User', 'view'),
+					('Delete', 'orange', 'User', 'delete'),
+				]
+			)
 
 
 class TeacherAdmission(QGroupBox):
@@ -220,8 +222,94 @@ class TeacherAdmission(QGroupBox):
 		self.formLayout.addWidget(self.password1, 3, 0)
 		self.formLayout.addWidget(self.password2, 3, 1)
 		
+		self.formButtonLayout = QVBoxLayout()
+		self.formButtonLayout.setAlignment(Qt.AlignCenter)
+
+		self.signUpLabel = QLabel('Already have an account?')
+		self.signUpLabel.setObjectName('signUpLabel')
+		self.signUpLabel.setMaximumSize(200, 50)
+		self.formButtonLayout.addWidget(self.signUpLabel)
+
+		self.signUpButton = QPushButton('Sign Up')
+		self.signUpButton.setStyleSheet(
+			'''
+				QPushButton {
+					background-color: blue;
+					color: white;
+				}
+				QPushButton:hover {
+					background-color: rgba(0, 0, 0, 0.05);
+				}
+			'''
+		)
+		self.signUpButton.clicked.connect(self.signUpButtonView)
+		self.signUpButton.setObjectName('admissionButton')
+		self.signUpButton.setMaximumSize(500, 50)
+		self.signUpButton.setSizePolicy(
+			QSizePolicy.Expanding,
+			QSizePolicy.Expanding
+		)
+		self.formButtonLayout.addWidget(self.signUpButton)
 
 		self.admissionCardContent.cardLayout.addLayout(self.formLayout)
+		self.admissionCardContent.cardLayout.addLayout(self.formButtonLayout)
+	
+	def signUpButtonView(self):
+		try:
+			phone_length = 10
+			password_length = 6
+			all_usernames = list(models.User.objects.values_list('username', flat=True))
+
+			if self.first_name.text() and self.last_name.text():
+				if self.username.text() not in all_usernames:
+					if len(self.phone_number.text()) > phone_length:
+						if self.email.text():
+							if len(self.password1.text()) > password_length and len(self.password2.text()) > password_length:
+								if self.password1.text() == self.password2.text():
+									new_user = models.User.objects.create(
+										first_name=self.first_name.text(),
+										last_name=self.last_name.text(),
+										username=self.username.text(),
+										email=self.email.text(),
+										phone=self.phone_number.text(),
+										is_teacher=True,
+									)
+
+									new_user.set_password(self.password1.text())
+									new_user.save()
+
+									message = f'Congratulations !!! \nYour registration was successful'
+									
+									self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+									self.messagePopUp.show()
+								else:
+									message = f'Password mismatch'
+									self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+									self.messagePopUp.show()
+							else:
+								message = f'Password length too short, At least six(6) characters are required'
+								self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+								self.messagePopUp.show()
+						else:
+							message = f'Enter a valid E-Mail address'
+							self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+							self.messagePopUp.show()
+					else:
+						message = f'Phone number must be at least ten(10) characters'
+						self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+						self.messagePopUp.show()
+				else:
+					message = f'{self.username.text()} already exist, try something different'
+					self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+					self.messagePopUp.show()
+			else:
+				message = f'First name and Last name is required'
+				self.messagePopUp = customWidgets.PopUp(title='School Manager', body=message, buttonText='&Ok')
+				self.messagePopUp.show()
+		except Exception as e:
+			message = str(e)
+			self.messagePopUp = customWidgets.PopUp(title='Error Manager', body=message, buttonText='&Ok')
+			self.messagePopUp.show()
 
 
 class TeacherPromotion(QGroupBox):
